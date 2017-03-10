bf9c4b26333e590d4aa6d0bd6f3ec283731d5f83
Kieran Turgoose <kieran.turgoose2@mail.dcu.ie>
2017-03-10 10:41:56 +0000

    Replace g_drive.py

--- code/motion_Code/g_drive.py
+++ code/motion_Code/g_drive.py
@@ -1,11 +1,17 @@
 from pyimagesearch.tempimage import TempImage
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+import datetime
 import conf
 
-import datetime
-
+# allows use of conf.json settings
 conf = conf.config()
+
+# initialise timestamp and temp image
+global timestamp
+temp = TempImage()
+timestamp = datetime.datetime.utcnow()
+ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
 
 #GoogleDrive Authentication
 gauth = GoogleAuth()
@@ -13,40 +19,56 @@ drive = GoogleDrive(gauth)
 
 def get_gauth():
     return gauth
+
 def get_drive():
     return drive
 
-def authentication():
-    if conf["auto_auth"]:
-            #Try to load saved client credentials
-            gauth.LoadCredentialsFile("mycreds.txt")
-            if gauth.credentials is None:
-                    gauth.LocalWebserverAuth()
-            elif gauth.access_token_expired:
-                    gauth.Refresh()
-            else:
-                    gauth.Authorize()
-            gauth.SaveCredentialsFile("mycreds.txt")
-    else:
-            gauth.LocalWebserverAuth()
-
-#Drive Upload
-temp = TempImage()
-global timestamp
-timestamp = datetime.datetime.utcnow()
-ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-            
 def get_ti():
     return temp
 
+def authentication():
+    # if user selects automatic authentication
+    if conf["auto_auth"]:
+            #Try to load saved client credentials
+            gauth.LoadCredentialsFile("mycreds.txt")
+
+            # authenticate through browser if first time running script
+            if gauth.credentials is None:
+                    gauth.LocalWebserverAuth()
+
+            # refresh the access token if it has expired        
+            elif gauth.access_token_expired:
+                    gauth.Refresh()
+
+            # authorize the user
+            else:
+                    gauth.Authorize()
+                    
+            # save user credentials for next run       
+            gauth.SaveCredentialsFile("mycreds.txt")
+            
+    # if user doesn't want their credentials saving
+    else:
+            gauth.LocalWebserverAuth()
+
+#Drive Upload         
 def uploader(ts):
-    file = drive.CreateFile()
-    file.SetContentFile(temp.path)
-    file['title'] = ts
-    file.Upload()
     global file_id
+
+    file = drive.CreateFile()
+
+    # set the contents of the file to be uploaded
+    file.SetContentFile(temp.path)
+
+    # edit the filename to the correct timestamp
+    file['title'] = ts
+
+    # upload file
+    file.Upload()
+    print('Created file %s with mimeType %s' % (file['title'], file['mimeType']))
+
+    # initialise file id to be used for sending link via SMS notification
     file_id = file.get('id')
-    print('Created file%s with mimeType %s' % (file['title'], file['mimeType']))
 
 def get_file_id():
     return file_id
